b7a5e4f565131bcf59faf3be18124c82c15fc9f3
rajibdas-it <rajibrad@gmail.com>
2022-06-09 19:49:50 +0600

    context wrap

--- src/component/App.js
+++ src/component/App.js
@@ -5,20 +5,23 @@ import Quiz from "./pages/Quiz";
 import Home from "./pages/Home";
 import Signup from "./pages/Signup";
 import Result from "./pages/Result";
+import { AuthProvider } from "../context/AuthContext";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <Router>
-      <Layout>
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route exact path="signup" element={<Signup />} />
-          <Route exact path="login" element={<Login />} />
-          <Route exact path="quiz" element={<Quiz />} />
-          <Route exact path="result" element={<Result />} />
-        </Routes>
-      </Layout>
+      <AuthProvider>
+        <Layout>
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route exact path="signup" element={<Signup />} />
+            <Route exact path="login" element={<Login />} />
+            <Route exact path="quiz" element={<Quiz />} />
+            <Route exact path="result" element={<Result />} />
+          </Routes>
+        </Layout>
+      </AuthProvider>
     </Router>
   );
 }
